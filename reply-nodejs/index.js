96a7073167e4d58a7febc0628b9c1ca05963106e
Pierre-Nick Durette <pndurette@gmail.com>
2019-09-12 17:08:30 -0400

    Update path

--- reply-nodejs/index.js
+++ reply-nodejs/index.js
@@ -1,21 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const client = new github.GitHub(
-    core.getInput('repo-token', {required: true})
-)
+async function run() {
+    const client = new github.GitHub(
+        core.getInput('repo-token', {required: true})
+    )
 
-const context = github.context
+    const context = github.context
 
-const comment = context.payload.comment.body
-const user    = context.payload.comment.user.login
+    const comment = context.payload.comment.body
+    const user    = context.payload.comment.user.login
 
-console.log(comment)
-console.log(user)
+    console.log(comment)
+    console.log(user)
 
-const newIssue = client.issues.createComment({
-    owner: context.payload.repository.owner.login,
-    repo: 'pndurette',
-    issue_number: 6,
-    body: 'POWER'
-})
+    const comment_payload = {
+        owner: context.payload.repository.owner.login,
+        repo: 'pndurette',
+        issue_number: 6,
+        body: 'POWER'
+    }
+
+    console.log(comment_payload)
+
+    const newIssue = await client.issues.createComment(comment_payload)
+}
